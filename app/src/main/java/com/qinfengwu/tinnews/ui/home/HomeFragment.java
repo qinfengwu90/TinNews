317d24d2ad88fb7e65fc400fc4ada8cb5f8ca065
Qinfeng Wu <qinfengwu90@gmail.com>
2022-12-09 11:13:28 -0700

    Added card stack view and like/dislike functions

--- app/src/main/java/com/qinfengwu/tinnews/ui/home/HomeFragment.java
+++ app/src/main/java/com/qinfengwu/tinnews/ui/home/HomeFragment.java
@@ -13,12 +13,22 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.qinfengwu.tinnews.R;
+import com.qinfengwu.tinnews.databinding.FragmentHomeBinding;
 import com.qinfengwu.tinnews.repository.NewsRepository;
 import com.qinfengwu.tinnews.repository.NewsViewModelFactory;
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager;
+import com.yuyakaido.android.cardstackview.CardStackListener;
+import com.yuyakaido.android.cardstackview.Direction;
+import com.yuyakaido.android.cardstackview.Duration;
+import com.yuyakaido.android.cardstackview.StackFrom;
+import com.yuyakaido.android.cardstackview.SwipeAnimationSetting;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements CardStackListener {
 
     private HomeViewModel viewModel;
+    private FragmentHomeBinding binding;
+    private CardStackLayoutManager layoutManager;
+    private CardSwipeAdapter swipeAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,12 +39,25 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        swipeAdapter = new CardSwipeAdapter();
+        layoutManager = new CardStackLayoutManager(requireContext(), this);
+        layoutManager.setStackFrom(StackFrom.Top);
+        binding.homeCardStackView.setLayoutManager(layoutManager);
+        binding.homeCardStackView.setAdapter(swipeAdapter);
+
+        binding.homeLikeButton.setOnClickListener(v -> {
+            swipeCard(Direction.Right);
+        });
+        binding.homeUnlikeButton.setOnClickListener(v -> {
+            swipeCard(Direction.Left);
+        });
 
         NewsRepository newsRepository = new NewsRepository();
         viewModel = new ViewModelProvider(
@@ -47,7 +70,51 @@ public class HomeFragment extends Fragment {
         viewModel.getTopHeadlines().observe(getViewLifecycleOwner(), newsResponse -> {
             if (newsResponse != null) {
                 Log.d("HomeFragment", newsResponse.toString());
+                swipeAdapter.setArticles(newsResponse.articles);
             }
         });
+    }
+
+    private void swipeCard(Direction direction) {
+        SwipeAnimationSetting setting = new SwipeAnimationSetting.Builder()
+                .setDirection(direction)
+                .setDuration(Duration.Normal.duration)
+                .build();
+        layoutManager.setSwipeAnimationSetting(setting);
+        binding.homeCardStackView.swipe();
+    }
+
+    @Override
+    public void onCardDragging(Direction direction, float v) {
+
+    }
+
+    @Override
+    public void onCardSwiped(Direction direction) {
+        if (direction == Direction.Left) {
+            Log.d("CardStackView", "Unliked " + layoutManager.getTopPosition());
+        } else if (direction == Direction.Right) {
+            Log.d("CardStackView", "Liked "  + layoutManager.getTopPosition());
+        }
+    }
+
+    @Override
+    public void onCardRewound() {
+
+    }
+
+    @Override
+    public void onCardCanceled() {
+
+    }
+
+    @Override
+    public void onCardAppeared(View view, int i) {
+
+    }
+
+    @Override
+    public void onCardDisappeared(View view, int i) {
+
     }
 }
